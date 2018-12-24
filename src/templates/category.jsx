4d501932fdca1dc4455f45fc2a5e59e48e5faff1
Kanako Abe <kanakoabe5@gmail.com>
2018-12-24 11:05:28 -0500

    Media list

--- src/templates/category.jsx
+++ src/templates/category.jsx
@@ -1,9 +1,23 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { Nav } from "../components/Nav"
+import { Body } from "../components/LayoutPrimitives"
 
 export default ({ data }) => {
-  const title = data.markdownRemark.frontmatter.title
-  return <div>{title}</div>
+  const { title, mediaList } = data.markdownRemark.frontmatter
+  return (
+    <Body>
+      <Nav />
+      {title}
+      {mediaList.map(media => (
+        <>
+          <h3>{media.title}</h3>
+          <img alt={media.title} src={media.media} />
+          <p>{media.caption}</p>
+        </>
+      ))}
+    </Body>
+  )
 }
 
 export const query = graphql`
@@ -12,6 +26,11 @@ export const query = graphql`
       html
       frontmatter {
         title
+        mediaList {
+          media
+          caption
+          title
+        }
       }
     }
   }
