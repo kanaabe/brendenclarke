84f592bc6023adf51615c80eea04b0043f5a0d9a
Kanako Abe <kanakoabe5@gmail.com>
2019-04-29 23:27:00 -0400

    jumbo

--- src/templates/category.jsx
+++ src/templates/category.jsx
@@ -11,18 +11,20 @@ export default ({ data }) => {
     <LayoutWrapper>
       <Body>
         <Nav />
-        <Work>
-          {mediaList.map(media => (
-            <React.Fragment>
-              <ImageContainer>
-                <Image alt={media.title} src={media.media} />
-              </ImageContainer>
-              <Caption>
-                <CaptionContent>{media.caption}</CaptionContent>
-              </Caption>
-            </React.Fragment>
-          ))}
-        </Work>
+        <WorkOuter>
+          <Work>
+            {mediaList.map(media => (
+              <MediaContainer>
+                <ImageContainer>
+                  <Image alt={media.title} src={media.media} />
+                </ImageContainer>
+                <Caption>
+                  <CaptionContent>{media.caption}</CaptionContent>
+                </Caption>
+              </MediaContainer>
+            ))}
+          </Work>
+        </WorkOuter>
       </Body>
     </LayoutWrapper>
   )
@@ -44,12 +46,23 @@ export const query = graphql`
   }
 `
 
-const Work = styled.div`
-  width: 100%;
+const MediaContainer = styled.div`
+  // width: 100vw;
   height: 100%;
   display: flex;
   flex-direction: column;
+`
+
+const WorkOuter = styled.div`
+  width: 100vw;
+  height: 100vh;
   overflow: scroll;
+`
+
+const Work = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
 `
 
 const ImageContainer = styled.div`
@@ -75,3 +88,33 @@ const CaptionContent = styled.p`
   margin: 0 20px 0 0;
   text-align: right;
 `
+
+// @keyframes ticker {
+//   0% { transform: translate3d(0, 0, 0); }
+//   100% { transform: translate3d(-100%, 0, 0); }
+// }
+// .tcontainer{
+//   width: 100%;
+//   overflow: hidden;
+// }
+// .ticker-wrap {
+//   width: 100%;
+//   padding-left: 100%;
+//   background-color: #eee;
+// }
+// .ticker-move {
+//   display: inline-block;
+//   white-space: nowrap;
+//   padding-right: 100%;
+//   animation-iteration-count: infinite;
+//   animation-timing-function: linear;
+//   animation-name: ticker;
+//   animation-duration: 10s;
+// }
+// .ticker-move:hover{
+//   animation-play-state: paused;
+// }
+// .ticker-item{
+//   display: inline-block;
+//   padding: 0 2rem;
+// }
